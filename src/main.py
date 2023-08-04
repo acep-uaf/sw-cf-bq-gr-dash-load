f76a1301c10de1ae796e2f65dc4715208837cdb3
jody <thisjody@gmail.com>
2023-08-04 07:25:40 -0700

    working post of dashboard stub 200

--- src/main.py
+++ src/main.py
@@ -9,19 +9,58 @@ import requests
 def load_gr_dash(event, context):
     print(f'Received event: {event}')  
     logging.info(f'Received event: {event}')
-    grafana_url = "http://10.138.0.26:3000/api/health" # Replace with your Grafana internal IP and port
+    #grafana_url = "http://10.138.0.26:3000/api/health" # Replace with your Grafana internal IP and port
+    create_dashboard_url = "http://10.138.0.26:3000/api/dashboards/db"
     secret_client = secretmanager.SecretManagerServiceClient()
     secret_name = f"projects/675499914510/secrets/sw-gr-token/versions/1"
     secret_response = secret_client.access_secret_version(name=secret_name)
     secret_value = secret_response.payload.data.decode('UTF-8')
-    headers = {'Authorization': f'Bearer {secret_value}'}
+    #headers = {'Authorization': f'Bearer {secret_value}'}
+    #headers = {'Authorization': f'Bearer {secret_value}', 'Content-Type': 'application/json'}
+    # Headers
+    headers = {'Accept': 'application/json','Content-Type': 'application/json','Authorization': f'Bearer {secret_value}'}
 
     try:
         # Test the connection to Grafana by checking its health
-        response = requests.get(grafana_url, headers=headers)
-        response.raise_for_status() # Will raise an HTTPError if the HTTP request returned an unsuccessful status code
+        #response = requests.get(grafana_url, headers=headers)
+        #response.raise_for_status() # Will raise an HTTPError if the HTTP request returned an unsuccessful status code
+        #print(f'Grafana Health Response: {response.json()}')
 
-        print(f'Grafana Health Response: {response.json()}')
+        # Define the URL to list folders
+        list_folders_url = "http://10.138.0.26:3000/api/folders"
+
+        # Send GET request to retrieve the folders
+        response = requests.get(list_folders_url, headers=headers)
+        response.raise_for_status()  # Check for errors
+
+        # Print the folders
+        folders = response.json()
+        for folder in folders:
+            print(f"ID: {folder['id']}, Title: {folder['title']}")
+     
+        # JSON Payload
+        payload = {
+            "dashboard": {
+                "title": "New Dashboard",
+                "schemaVersion": 16,
+                "version": 0
+            },
+            "folderId": 0,
+            "overwrite": False
+        }
+
+        # Send POST request
+        response = requests.post(create_dashboard_url, json=payload, headers=headers)
+
+        print(f'Status Code: {response.status_code}')
+        print(f'Response: {response.text}')  
+
+        # Check for errors
+        response.raise_for_status()
+
+        # Print or return the response
+        print(response.json())
+     
 
         pubsub_message = base64.b64decode(event['data']).decode('utf-8')
         message_dict = json.loads(pubsub_message)
@@ -42,7 +81,6 @@ def load_gr_dash(event, context):
 
         print(f'json_content : {json_content}')
 
-        
 
  
         
