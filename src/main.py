f0cfc418e6ccfc19fae2173c46c06f8cdf616a2b
jody <thisjody@gmail.com>
2023-09-20 15:40:31 -0700

    setting overwrite true

--- src/main.py
+++ src/main.py
@@ -9,27 +9,14 @@ import os
 def load_gr_dash(event, context):
     print(f'Received event: {event}')  
     logging.info(f'Received event: {event}')
-    #grafana_url = "http://10.138.0.26:3000/api/health" # Replace with your Grafana internal IP and port
-    #create_dashboard_url = "http://10.138.0.26:3000/api/dashboards/db"
     create_dashboard_url = os.environ.get('CREATE_DASHBOARD_URL')
     secret_client = secretmanager.SecretManagerServiceClient()
-    #secret_name = f"projects/675499914510/secrets/sw-gr-token/versions/1"
     secret_name = os.environ.get('SECRET_NAME')
     secret_response = secret_client.access_secret_version(name=secret_name)
     secret_value = secret_response.payload.data.decode('UTF-8')
-    #headers = {'Authorization': f'Bearer {secret_value}'}
-    #headers = {'Authorization': f'Bearer {secret_value}', 'Content-Type': 'application/json'}
-    # Headers
     headers = {'Accept': 'application/json','Content-Type': 'application/json','Authorization': f'Bearer {secret_value}'}
 
     try:
-        # Test the connection to Grafana by checking its health
-        #response = requests.get(grafana_url, headers=headers)
-        #response.raise_for_status() # Will raise an HTTPError if the HTTP request returned an unsuccessful status code
-        #print(f'Grafana Health Response: {response.json()}')
-
-        # Define the URL to list folders
-        #list_folders_url = "http://10.138.0.26:3000/api/folders"
         list_folders_url = os.environ.get('LIST_FOLDERS_URL')
         # Send GET request to retrieve the folders
         response = requests.get(list_folders_url, headers=headers)
@@ -64,7 +51,7 @@ def load_gr_dash(event, context):
         payload = {
             "dashboard": json_content,
             "folderId": 3,
-            "overwrite": False
+            "overwrite": True
         }
 
         # Send POST request
